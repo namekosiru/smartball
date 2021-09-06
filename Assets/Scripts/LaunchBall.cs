ca742a69d2192bcd3b2d524b6e1d097a3538bda3
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-06 13:54:30 +0900

    ランダムにボール選択機能の追加

--- Assets/Scripts/LaunchBall.cs
+++ Assets/Scripts/LaunchBall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LaunchBall : MonoBehaviour
 {
     public GameObject launchpoint;
-    public GameObject ball;
+    public GameObject[] ballPrefabs;
     public float speed;
 
     // Start is called before the first frame update
@@ -28,11 +28,17 @@ public class LaunchBall : MonoBehaviour
         // 弾を発射する場所を取得
         Vector3 ballPosition = launchpoint.transform.position;
         // 上で取得した場所に、"bullet"のPrefabを出現させる
-        GameObject newBall = Instantiate(ball, ballPosition, transform.rotation);
+        GameObject newBall = Instantiate(SelectBall(), ballPosition, transform.rotation);
         // 出現させたボールのforward(z軸方向)
         Vector3 direction = newBall.transform.forward;
         // 弾の発射方向にnewBallのz方向(ローカル座標)を入れ、弾オブジェクトのrigidbodyに衝撃力を加える
         newBall.GetComponent<Rigidbody>().AddForce(direction * speed, ForceMode.Impulse);
   
+    }
+
+    GameObject SelectBall()
+    {
+        int index = Random.Range(0, ballPrefabs.Length);
+        return ballPrefabs[index];
     }
 }
